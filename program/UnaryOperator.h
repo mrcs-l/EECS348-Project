059d8efdbadf6f6ac8b884d150c81689913f4f21
Marcos Lepage <mlepage@ku.edu>
2023-11-29 19:31:57 -0600

    added UnaryOperator.h

---
+++ program/UnaryOperator.h
@@ -0,0 +1,14 @@
+#ifndef UNARYOPERATOR_H
+#define UNARYOPERATOR_H
+
+#include "Operator.h"
+
+class UnaryOperator : public Operator {
+
+	public:
+		int params = 1;
+		
+		virtual float Calculate(float exp) = 0;
+};
+
+#endif
