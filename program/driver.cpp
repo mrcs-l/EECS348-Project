64e4496551fe827b13934a12145ea4401c8f2656
tdathon <tdathon@ku.edu>
2023-11-30 16:31:33 -0600

    fixed main()

--- program/driver.cpp
+++ program/driver.cpp
@@ -2,9 +2,10 @@
 #include "lexer.h"
 #include "AST.h"
 #include "Calculator.h"
+#include "Parser.h"
 
 int main() {
 	string expression = lexer::lex();
-	AST ast = parser::parse(expression);
+	AST ast = Parser::parse(expression);
 	return Calculator::evaluate(ast);
 }
